6b6e0ab05a0f4608aeea7932697cd0916297733d
Nesho Neshev <neshev1978@gmail.com>
2020-12-30 09:41:05 +0200

    Add files via upload

---
+++ Arrays/01. Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] trains = new int[n];
+            int result = 0;
+            for (int i = 0; i < trains.Length; i++)
+            {
+                trains[i] = int.Parse(Console.ReadLine());
+                result += trains[i];
+            }
+
+            Console.WriteLine(string.Join(" ", trains));
+            Console.WriteLine(result);
+        }
+    }
+}
