21e57db1209c5429c54128d618dff39f6bf36109
Nesho Neshev <neshev1978@gmail.com>
2020-12-30 11:04:35 +0200

    Add files via upload

---
+++ Regular Expressions/01. Furniture/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @">>(?<furniture>[A-Z-a-z]+)<<(?<price>[\d+\.?\d*]+)!(?<quantity>\d+)";
+            List<string> furniture = new List<string>();
+            double price = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Purchase")
+                {
+                    break;
+                }
+                var match = Regex.Match(input, patern);
+                if (match.Success)
+                {
+                    furniture.Add(match.Groups[1].Value);
+                    price += double.Parse(match.Groups[2].Value) *
+                             double.Parse(match.Groups[3].Value);
+                }
+            }
+            Console.WriteLine("Bought furniture:");
+            if (furniture.Count > 0)
+            {
+
+                Console.WriteLine(string.Join(Environment.NewLine, furniture));
+            }
+            Console.WriteLine($"Total money spend: {price:f2}");
+        }
+    }
+}
+
+
---
+++ Regular Expressions/02. Race/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namePattern = @"([A-Za-z])";
+            string pointsPattern = @"\d";
+            Dictionary<string, int> personKm = new Dictionary<string, int>();
+            List<string> person = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end of race")
+                {
+                    break;
+                }
+                var nameMatch = Regex.Matches(input, namePattern);
+                var kmMatch = Regex.Matches(input, pointsPattern);
+                var name = string.Concat(nameMatch);
+                var sum = kmMatch.Select(x => int.Parse(x.Value)).Sum();
+                if (person.Contains(name))
+                {
+                    if (!personKm.ContainsKey(name))
+                    {
+                        personKm.Add(name, sum);
+                    }
+                    else
+                    {
+                        personKm[name] += sum;
+                    }
+                }
+            }
+            var sorted = personKm.OrderByDescending(x => x.Value).Select(x => x.Key).ToList();
+            Console.WriteLine($"1st place: {sorted[0]}");
+            Console.WriteLine($"2nd place: {sorted[1]}");
+            Console.WriteLine($"3rd place: {sorted[2]}");
+        }
+    }
+}
+
+
